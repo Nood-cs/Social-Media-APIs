8ff7b057370dc4f4f467eeab73da708d9d8d4dec
Nada Medhat <nada.medhat@outlook.com>
2023-05-05 02:35:35 +0300

    APIs with Raw SQL statements

--- app/main.py
+++ app/main.py
@@ -47,70 +47,72 @@ def root():
 def get_posts():
     cur.execute("select * from posts;")
     posts = cur.fetchall()
-    print(posts)
+    
     return {"data": posts}
 
-# @app.post("/createpost")
-# def create_post(payload: dict = Body(...)):
-#     print(payload)
-#     return {"new post": f"{payload['Title']}, {payload['Content']}"}
-# title str, content str
-
-
-@app.post("/createpost")
-def createpost(new_post: Post):
-    print(new_post)
-    post_dict = new_post.dict()
-    print(post_dict)
-    return {"data": post_dict}
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def new_post(post: Post):
-    new_post = post.dict()
-    new_post['id'] = randrange(0, 1000000)
-    my_posts.append(new_post)
+
+    cur.execute("INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *", 
+                (post.title, post.content, post.published))
+    
+    new_post = cur.fetchone()
+
+    conn.commit()
+
     return {"data": new_post}
 
 
 @app.get("/posts/latest")
 def get_latest_post():
-    post = my_posts[len(my_posts)- 1]
-    print(post)
+    
+    cur.execute("SELECT * FROM posts ORDER BY created_at DESC LIMIT 1;")
+    post = cur.fetchone()
+
     return {"details" : post}
+
 
 @app.get("/posts/{id}")
 def get_post(id : int):
-    post = find_post(id)
+
+    cur.execute("SELECT * FROM posts WHERE id = %s;",(str(id)))
+    post = cur.fetchone()
     
     if not post:
         raise HTTPException(status_code= status.HTTP_404_NOT_FOUND,
                              detail=f"post with id {id} was not found!")
-    print(post)
+    
     return {"post_details": post}
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id : int):
 
-    index = find_post_index(id)
-    print(index)
-    if index is None:
-        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, 
-                            detail= f"post with id {id} was not found!")
-    my_posts.pop(index)
+    cur.execute("DELETE FROM posts WHERE id = %s RETURNING *;", (str(id)))
+        
+    post = cur.fetchone()
+    conn.commit()
+
+    if post is None:
+        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id {id} was not found!")
+   
     return Response(status_code=status.HTTP_204_NO_CONTENT)
     
 
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
    
-   index = find_post_index(id)
-
-   if index == None:
-       raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, 
-                            detail= f"post with id {id} was not found!")
-   
-   post_dict = post.dict() # has not id
-   post_dict['id'] = id
-   my_posts[index] = post_dict
-   return {"detail" : post_dict}
+    cur.execute("UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *;", 
+                 (post.title, post.content, post.published, (str(id))))
+        
+    post_dict = cur.fetchone()
+    
+    conn.commit()
+    
+    if post_dict is None:
+        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND,
+                             detail=f"post with id {id} was not found!")
+    
+    return {"detail" : post_dict}
