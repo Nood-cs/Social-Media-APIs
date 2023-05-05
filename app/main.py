396dcf88d0ed55538049cb99064de36100944f03
Nada Medhat <nada.medhat@outlook.com>
2023-05-05 22:13:04 +0300

    using SQLAlchemy to connet with postgreSQL

--- app/main.py
+++ app/main.py
@@ -1,12 +1,26 @@
 from typing import Optional
-from fastapi import Body, FastAPI, Response, status, HTTPException
+from fastapi import Body, FastAPI, Response, status, HTTPException, Depends
 from pydantic import BaseModel
 from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+from sqlalchemy.orm import Session
+from . import models
+from .database import engine, SessionLocal
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 class Post(BaseModel):
@@ -14,16 +28,16 @@ class Post(BaseModel):
     content: str
     published: bool = True
 
-while True:
-    try:
-        conn = psycopg2.connect(host = "127.0.0.1",dbname = "fastapi", user = "postgres", 
-                            password = "test123", cursor_factory=RealDictCursor)
-        cur = conn.cursor()
-        print("Database connected successfully!")
-        break
-    except Exception as error:
-        print("Database connection error ", error)
-        time.sleep(2)
+# while True:
+#     try:
+#         conn = psycopg2.connect(host = "localhost",dbname = "fastapi", user = "postgres", 
+#                             password = "test123", cursor_factory=RealDictCursor)
+#         cur = conn.cursor()
+#         print("Database connected successfully!")
+#         break
+#     except Exception as error:
+#         print("Database connection error ", error)
+#         time.sleep(2)
 
 my_posts =[{"title": "Helllooo", "content": "I love pizza", "id":1},{"title": "Hello Nada", "content":"How are you?", "id":2}]
 
@@ -42,6 +56,12 @@ def find_post_index(id):
 @app.get("/")
 def root():
     return {"message": "Welcome to my API"}
+
+
+@app.get("/sqlalchemy")
+def test_posts(db: Session = Depends(get_db)):
+    return {"status" : "Success"}
+
 
 @app.get("/posts")
 def get_posts():
