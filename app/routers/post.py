3c9fa101a07c2c83f2323d36ee4236fcea0716c4
Nada Medhat <nada.medhat@outlook.com>
2023-08-27 02:22:39 +0300

    Added Relationship and OAuth related updates

--- app/routers/post.py
+++ app/routers/post.py
@@ -12,15 +12,17 @@ router = APIRouter(
 
 
 @router.get("/", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db), user_id : int = Depends(oauth2.get_current_user)):
+def get_posts(db: Session = Depends(get_db), curr_user : int = Depends(oauth2.get_current_user)):
     # cur.execute("select * from posts;")
     # posts = cur.fetchall()
+    print(curr_user.email)
+
     posts = db.query(models.Post).all()
     return posts
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model= schemas.Post)
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), user_id : int = Depends(oauth2.get_current_user)):
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), curr_user : int = Depends(oauth2.get_current_user)):
 
     # cur.execute("INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *", 
     #             (post.title, post.content, post.published))
@@ -30,7 +32,7 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), user_i
     # conn.commit()
 
     # print(post.dict())
-    print(user_id)
+    print(curr_user.email)
 
     new_post = models.Post(**post.dict())
     
@@ -41,10 +43,11 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), user_i
 
 
 @router.get("/latest", response_model=schemas.Post)
-def get_latest_post(db: Session = Depends(get_db), user_id : int = Depends(oauth2.get_current_user)):
+def get_latest_post(db: Session = Depends(get_db), curr_user : int = Depends(oauth2.get_current_user)):
     
     # cur.execute("SELECT * FROM posts ORDER BY created_at DESC LIMIT 1;")
     # post = cur.fetchone()
+    print(curr_user.email)
 
     post = db.query(models.Post).order_by(desc(models.Post.id)).limit(1).first()
 
@@ -52,10 +55,11 @@ def get_latest_post(db: Session = Depends(get_db), user_id : int = Depends(oauth
 
 
 @router.get("/{id}", response_model=schemas.Post)
-def get_post(id : int, db: Session = Depends(get_db), user_id : int = Depends(oauth2.get_current_user)):
+def get_post(id : int, db: Session = Depends(get_db), curr_user : int = Depends(oauth2.get_current_user)):
 
     # cur.execute("SELECT * FROM posts WHERE id = %s;",(str(id)))
     # post = cur.fetchone()
+    print(curr_user.email)
 
     post = db.query(models.Post).filter(models.Post.id == id).first()
     
@@ -67,12 +71,13 @@ def get_post(id : int, db: Session = Depends(get_db), user_id : int = Depends(oa
 
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id : int, db: Session = Depends(get_db), user_id : int = Depends(oauth2.get_current_user)):
+def delete_post(id : int, db: Session = Depends(get_db), curr_user : int = Depends(oauth2.get_current_user)):
 
     # cur.execute("DELETE FROM posts WHERE id = %s RETURNING *;", (str(id)))
         
     # post = cur.fetchone()
     # conn.commit()
+    print(curr_user.email)
 
     post = db.query(models.Post).filter(models.Post.id == id)
     
@@ -87,7 +92,7 @@ def delete_post(id : int, db: Session = Depends(get_db), user_id : int = Depends
     
 
 @router.put("/{id}", response_model=schemas.Post)
-def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db), user_id : int = Depends(oauth2.get_current_user)):
+def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db), curr_user : int = Depends(oauth2.get_current_user)):
    
     # cur.execute("UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *;", 
     #              (post.title, post.content, post.published, (str(id))))
@@ -95,6 +100,8 @@ def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)
     # post_dict = cur.fetchone()
     
     # conn.commit()
+
+    print(curr_user.email)
     
     post_query = db.query(models.Post).filter(models.Post.id == id)
 
